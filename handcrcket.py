43b6d5a89ae7c985996d2550eee896991979a299
thisuser562 <151726715+thisuser562@users.noreply.github.com>
2023-11-22 15:39:53 +0530

    Update handcrcket.py

--- handcrcket.py
+++ handcrcket.py
@@ -1,6 +1,6 @@
 import  string
 import  random
-choice=input("Enter  your  choice  (odd/even)")
+choise=input("Enter  your  choice  (odd/even)")
 a=int(input("  enter  a  random  number  between  1-6"))
 b=random.randint(1,6)
 S=a+b
@@ -10,6 +10,10 @@ elif(S%2!=0 and  choice=="odd"):
   print("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??")
 else:
   print("you  lost  the  toss")
+  cric=["batting","bowling"]
+  soos=random.choice(cric)
+  print(f"The  computer  has  chose  to  {soos}")
+
  
   
   
