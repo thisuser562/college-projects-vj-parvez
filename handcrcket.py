7e96de3ff4ec9b405a19162ed154642d11254e54
thisuser562 <151726715+thisuser562@users.noreply.github.com>
2023-11-22 15:29:52 +0530

    Create handcrcket.py

---
+++ handcrcket.py
@@ -0,0 +1,15 @@
+import  string
+import  random
+choice=input("Enter  your  choice  (odd/even)")
+a=int(input("  enter  a  random  number  between  1-6"))
+b=random.randint(1,6)
+S=a+b
+if(S%2==0 and  choice=="even"):
+  print("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??")
+elif(S%2!=0 and  choice=="odd"):
+  print("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??")
+else:
+  print("you  lost  the  toss")
+ 
+  
+  
