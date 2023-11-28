63214134e2ff6625190ccef78b1d0878024bd060
thisuser562 <151726715+thisuser562@users.noreply.github.com>
2023-11-28 11:32:26 +0530

    Update handcrcket.py
    made-a-few-changes-on-how-the-whole-thing-works...

--- handcrcket.py
+++ handcrcket.py
@@ -1,14 +1,23 @@
 import  string
 import  random
 choice=input("Enter  your  choice  (odd/even):  ")
+while(choice!="odd"  and  choice!="even"):
+    print("invalid")
+    choice=input("Enter  your  choice  (odd/even):  ")
 a=int(input("  enter  a  random  number  between  1-6:  "))
+#incase wrong-entry
+while(a>=7):
+    print("invalid  no.")
+    a=int(input("  enter  a  random  number  between  1-6:  "))
 b=random.randint(1,6)
 print(f"computer choice {b}")
 S=a+b
 if(S%2==0 and  choice=="even"):
-  print("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  toss=input("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  print(f"You  are {toss} first!")
 elif(S%2!=0 and  choice=="odd"):
-  print("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  toss=input("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  print(f"You   are {toss} first!")
 else:
   print("you  lost  the  toss")
   cric=["batting","bowling"]
@@ -23,13 +32,7 @@ for  i  in  range(0,11,1):
     print("golden  duck")
     break
   elif(j==c):
-    SUM+=j
     print(f"your  score  is  {SUM}")
     break
   else:
     SUM+=j
-  
-
- 
-  
-  
